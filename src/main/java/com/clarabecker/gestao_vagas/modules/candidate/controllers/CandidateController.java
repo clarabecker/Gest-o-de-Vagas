a90917c2b4c7afbe1d4b552edc60ca5272178c8c
clarabecker <Claraeu15@outlook.com>
2024-08-24 15:13:22 -0300

    Creation of Entity Candidate and validations.

---
+++ src/main/java/com/clarabecker/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,26 @@
+package com.clarabecker.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.clarabecker.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/candidate")
+
+public class CandidateController {
+    
+    @PostMapping("/")
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato:");
+        System.out.println(candidateEntity.getEmail());
+       
+    }
+    
+
+}
