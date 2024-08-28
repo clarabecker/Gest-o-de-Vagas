8b0ac2ca497e43a501bd91be1995fe21d23c9ce2
clarabecker <Claraeu15@outlook.com>
2024-08-27 22:15:54 -0300

    creation of candidate repository

--- src/main/java/com/clarabecker/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/clarabecker/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,10 +2,16 @@ package com.clarabecker.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clarabecker.gestao_vagas.exceptions.UserFoundException;
 import com.clarabecker.gestao_vagas.modules.candidate.CandidateEntity;
+import com.clarabecker.gestao_vagas.modules.candidate.CandidateRepository;
+import com.clarabecker.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,12 +20,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/candidate")
 
 public class CandidateController {
+
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
     
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato:");
-        System.out.println(candidateEntity.getEmail());
-       
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try{
+            var result = this.createCandidateUseCase.execute(candidateEntity);  
+            return ResponseEntity.ok().body(result);
+        }
+        catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        
     }
     
 
